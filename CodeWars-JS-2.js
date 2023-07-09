4a4baa0ff4119fc83c50465717c70f40f065f83c
unknown <79helloworld@gmail.com>
2023-07-09 21:42:38 +0200

    new problem solution

---
+++ CodeWars-JS-2.js
@@ -0,0 +1,11 @@
+/* Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents.
+With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! 
+He wants you to write a simple program telling him if he will be able to fit all the passengers. */
+
+function enough(cap, on, wait) {
+    if((cap - on - wait) < 0) {
+        return Math.abs(cap - on - wait);
+    } else {
+        return 0;
+    }
+}
